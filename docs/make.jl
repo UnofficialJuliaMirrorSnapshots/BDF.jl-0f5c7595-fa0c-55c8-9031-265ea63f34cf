5beeae29dfa723fbced85c39b202c767f9051e9a
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 19:09:27 +0000

    Snapshot of branch master taken on 2019-04-07T15:09:26.026-04:00 from "https://github.com/UnofficialJuliaMirror/BDF.jl-0f5c7595-fa0c-55c8-9031-265ea63f34cf"

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+using Documenter, DocumenterMarkdown, BDF
+
+if ispath("build/")
+    rm("build/", recursive=true)
+end
+
+if ispath("site/")
+    rm("site/", recursive=true)
+end
+
+include("runWeave.jl") #convert weave file to markdown
+makedocs(format = Markdown(), modules = [BDF], source="src", build="build")
+run(`mkdocs build`)
